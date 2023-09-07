8f53236f2925a65e7f25a7653c0d162c041c3bd2
jolade okunlade <jokunlade@gmail.com>
2023-09-07 21:05:06 +0100

    testing react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React 101</h1>
+      <img src="" alt="" />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora nobis tempore fugit.
+         Vitae temporibus, error aliquam maxime nemo obcaecati eius ipsum tempora,
+          culpa perspiciatis quae eveniet aut quas eaque accusamus.</p>
+    </div>
+  );
+}
+
+export default App;
